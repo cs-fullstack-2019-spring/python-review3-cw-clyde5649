92b1d5135b93118505db79ef7c9ea9fda931bb36
clyde5649 <clyde.bledsoe@yahoo.com>
2019-01-28 15:04:41 -0600

    go

---
+++ classwork.py
@@ -0,0 +1,44 @@
+def main():
+    # problem1()
+    # problem2()
+
+
+
+#give a number n and return true if n is
+
+
+
+def problem2()
+
+
+def in1to10(n , outside_mode)
+
+     if n <= 10
+      outside_mode = true
+
+
+    if n >= 10
+      outside_mode = true
+
+
+
+
+
+def problem2():
+
+
+   Userinput = ("enter all your games")
+
+   if (Userinput != "equal"):
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
